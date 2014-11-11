8a7da03cf54d8fb0eb694ce0e641d3f6af3659ef
aaronjudd <aaronjudd@me.com>
2014-11-11 13:28:23 -0800

    Version 1.0.0

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name: "ongoworks:speakingurl",
   summary: "Generate a slug. Wraps the speakingurl package for Meteor",
-  version: "0.1.0",
+  version: "1.0.0",
   git: "https://github.com/ongoworks/meteor-speakingurl"
 });
 
